3362bb1d054ce727f725a1256196f1fda8bd5fc7
Julie <juliesof@gmail.com>
2014-06-18 19:18:51 -0400

    Added 20 rows with data for each of the schema

--- bank_homework.sql
+++ bank_homework.sql
@@ -29,7 +29,25 @@ ALTER TABLE payments ADD zipcode INTEGER;
 -- try to keep the accounts positive and have at least three different payers.
 INSERT INTO payments () VALUES
   ('2014-05-01', 10.00, 'Moe', 'Larry', 'desposit'),
-  ();
+  ('2014-05-02', -5.00, 'Sarah', 'Larry', 'payment'),
+  ('2014-05-03', 20.00, 'Sarah', 'Moe', 'desposit'),
+  ('2014-05-04', 10.00, 'Larry', 'Sarah', 'desposit'),
+  ('2014-05-05', 60.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-06', -10.00, 'Moe', 'Larry', 'payment'),
+  ('2014-05-07', 1.00, 'Sarah', 'Jitsi', 'desposit'),
+  ('2014-05-08', -10.00, 'Larry', 'Moe', 'payment'),
+  ('2014-05-09', 30.00, 'Jitsi', 'Sarah', 'payment'),
+  ('2014-05-10', 17.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-11', -23.00, 'Chava', 'Larry', 'payment'),
+  ('2014-05-12', 1.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-13', 13.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-14', -14.00, 'Chava', 'Sarah', 'payment'),
+  ('2014-05-15', 10.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-16', 45.00, 'Larry', 'Jitsi', 'desposit'),
+  ('2014-05-17', 10.00, 'Moe', 'Larry', 'desposit'),
+  ('2014-05-18', -10.00, 'Jitsi', 'Chava', 'payment'),
+  ('2014-05-19', 10.00, 'Sarah', 'Larry', 'desposit'),
+  ('2014-05-20', 42.00, 'Moe', 'Larry', 'desposit');
 
 -- You receive notice that the payment with id of 9 is $10 too low.
 -- Alter that row so that it is corrected
